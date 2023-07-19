fadd097f18f223a854c2d0801cc2650130d15c75
Andreas Jim <ajimhartm@ethz.ch>
2023-07-19 16:45:17 +0200

    refact: Use global instead of local error handling

--- src-tauri/src/commands/build_tar.rs
+++ src-tauri/src/commands/build_tar.rs
@@ -3,16 +3,11 @@ use brotli::CompressorWriter;
 use data_encoding::HEXLOWER;
 use sha2::{Digest, Sha256};
 use std::fs::File;
+use std::io;
 use std::path::Path;
 
-#[tauri::command]
-pub fn build_tar(
-    file_name: String,
-    root_dir: String,
-    files: Vec<String>,
-) -> Result<String, String> {
-    let file =
-        File::create(file_name).map_err(|e| format!("Could not create archive file: {e}"))?;
+fn build_tar_io(file_name: String, root_dir: String, files: Vec<String>) -> io::Result<String> {
+    let file = File::create(file_name)?;
 
     let brotli = CompressorWriter::new(file, 4096, 11, 20);
     let hasher = Sha256::new();
@@ -22,17 +17,23 @@ pub fn build_tar(
     archive.follow_symlinks(true);
 
     let root_dir = Path::new(&root_dir);
+
     for x in files {
         let abs_path = root_dir.join(&x);
         eprintln!("adding file '{}' with name '{}'", abs_path.display(), &x);
-        archive
-            .append_path_with_name(&abs_path, &x)
-            .map_err(|e| format!("Could not add file to archive: {e}"))?;
+        archive.append_path_with_name(&abs_path, &x)?
     }
 
-    let tee = archive
-        .into_inner()
-        .map_err(|e| format!("Could not close archive: {e}"))?;
+    let tee = archive.into_inner()?;
     let (_, hasher) = tee.into_inner();
     Ok(HEXLOWER.encode(hasher.finalize().as_ref()))
+}
+
+#[tauri::command]
+pub fn build_tar(
+    file_name: String,
+    root_dir: String,
+    files: Vec<String>,
+) -> Result<String, String> {
+    build_tar_io(file_name, root_dir, files).map_err(|e| format!("Could not build archive: {e}"))
 }
