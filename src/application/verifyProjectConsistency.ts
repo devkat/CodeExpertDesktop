20a42fb28f3246b9ba2e2f8b3c504d85d2241ccd
Andreas Jim <ajimhartm@ethz.ch>
2023-07-19 16:45:32 +0200

    refact: Simplify code

--- src/application/verifyProjectConsistency.ts
+++ src/application/verifyProjectConsistency.ts
@@ -1,4 +1,4 @@
-import { boolean, constVoid, pipe, taskEither } from '@code-expert/prelude';
+import { either, pipe, task, taskEither } from '@code-expert/prelude';
 import { LocalProject } from '@/domain/Project';
 import { SyncException, syncExceptionADT } from '@/domain/SyncException';
 import { exists } from '@/lib/tauri/fs';
@@ -7,12 +7,13 @@ export const verifyProjectConsistency = ({
   value: { basePath },
 }: LocalProject): taskEither.TaskEither<SyncException, void> =>
   pipe(
-    taskEither.fromTask(exists(basePath)),
-    taskEither.filterOrElse(boolean.isTrue, () =>
-      syncExceptionADT.fileSystemCorrupted({
-        path: basePath,
-        reason: 'Project directory does not exist.',
-      }),
+    exists(basePath),
+    task.map(
+      either.guard(() =>
+        syncExceptionADT.fileSystemCorrupted({
+          path: basePath,
+          reason: 'Project directory does not exist.',
+        }),
+      ),
     ),
-    taskEither.map(constVoid),
   );
